6f44305711e5432f3de0596e699afe58afdbeb18
Sheila Leveille <sbleveille@gmail.com>
2020-10-28 03:35:10 -0400

    FEAT: syntax spacing

--- app.rb
+++ app.rb
@@ -5,21 +5,30 @@ require "sinatra/activerecord"
 require "pry"
 
 # verb 'url' do
+# get == get information on a page
+# post == post information to something (ex. DB)
+
+# params is a hash that can carry info from one web page to another
 
 get '/' do
   @characters = Character.all
+
+  # display the index.erb page in the views folder
   erb :index
 end
 
 get '/characters/:id' do
-# params = { id: 1 }
+  # params = { id: 1 }
+
   @character = Character.find(params[:id])
+
+  # display the show.erb page in the views folder
   erb :show
 end
 
-# params = {"name"=>"Harry Potter", "address"=>"Hogwarts"}
 post '/characters' do
-  # Character.create
+  # params = {"name"=>"Harry Potter", "address"=>"Hogwarts"}
+
   character = Character.new(
     name: params['name'],
     address: params['address']
